cee4b3095f66524788eb8d08fa6cecf145e23db4
akhila-ariyachandra <akhila_ariyachandra@live.com>
2019-11-18 16:17:42 +0530

    Start customizing base theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,41 @@
 require(`dotenv`).config({
-  path: `.env`,
-})
+  path: `.env`
+});
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitle: `Akhila Ariyachandra`,
+    siteTitleAlt: `Akhila Ariyachandra`,
     navigation: [
       {
         title: `Blog`,
-        slug: `/blog`,
+        slug: `/blog`
       },
       {
         title: `About`,
-        slug: `/about`,
-      },
-    ],
+        slug: `/about`
+      }
+    ]
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        head: true,
+        cookieDomain: `akhilaariyachandra.com`,
+        optimizeId: process.env.GOOGLE_OPTIMIZE_ID
+      }
     },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        name: `Akhila Ariyachandra's Blog`,
         short_name: `minimal-blog`,
         description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
@@ -42,17 +46,16 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
-}
+    `gatsby-plugin-offline`
+  ]
+};
