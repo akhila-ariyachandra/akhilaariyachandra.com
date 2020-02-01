fe148f9443fbb86e1602daa1580438d91befd6c2
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-02-02 01:00:50 +0530

    Delete default css file

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-//import "./layout.css"
 
 const Layout = ({ children, location }) => {
   const data = useStaticQuery(graphql`
