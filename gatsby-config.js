ff13ed7288d6507e7c4b55f7fd84044b3629e1fe
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-02-04 12:53:19 +0530

    Disable links in markdown images

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,7 @@ module.exports = {
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 960,
+              linkImagesToOriginal: false,
             },
           },
           {
