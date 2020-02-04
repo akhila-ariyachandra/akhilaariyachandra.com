8088a0e9c90bd5dd6f4be58304b31267e4a19271
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-02-04 19:31:45 +0530

    Added Algolia backend for searching

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+const queries = require("./src/utils/algolia")
 require(`dotenv`).config({
   path: `.env`,
 })
@@ -84,6 +85,15 @@ module.exports = {
             options: {},
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries,
+        chunkSize: 10000, // default: 1000
       },
     },
   ],
