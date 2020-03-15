4c6cd5bf0af487707643dc8a825dea9bd04d7988
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-03-15 20:40:35 +0530

    Fix syntax highlighting

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 type Props = {
   language?: string;
