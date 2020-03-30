c7fea881e6811b0185971aea327a91e6bf6c556d
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-03-31 00:04:03 +0530

    Delete old files

--- src/util/cms.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "contentful";
-
-export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-});
