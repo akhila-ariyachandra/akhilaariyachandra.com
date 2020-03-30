6681679fb32b2e890c8b4470e49a5a833f7e88ec
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-03-30 21:43:31 +0530

    Fix import

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 
-import Bio from "../components/bio";
+import Bio from "../components/Bio";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import { rhythm, scale } from "../utils/typography";
