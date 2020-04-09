a34d31fb49b6ef46cb2e33a0c95042a7eec86cc6
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-09 21:55:45 +0530

    Setup Purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
-module.exports = () => ({
-  plugins: [require("tailwindcss")],
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.js"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
