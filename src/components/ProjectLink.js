495e5ad447ba76cf4427f7873fc535682596d205
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-12 18:29:59 +0530

    Removed changing background color on hover

--- src/components/ProjectLink.js
+++ src/components/ProjectLink.js
@@ -4,7 +4,7 @@ import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const ProjectLink = ({ project }) => {
   return (
-    <article className="p-3 rounded-md shadow-md bg-white transition duration-500 transform hover:text-white hover:antialiased hover:scale-105 hover:shadow-xl hover:bg-gray-700">
+    <article className="p-3 rounded-md shadow-md bg-white transition duration-500 transform hover:scale-105 hover:shadow-xl">
       <h3 className="text-2xl font-semibold">
         <OutboundLink
           href={project.url}
