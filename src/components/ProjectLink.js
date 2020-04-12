81810015c0025f57c57c00b35cb8317bc5af53be
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-12 18:30:50 +0530

    Make entire block clickable

--- src/components/ProjectLink.js
+++ src/components/ProjectLink.js
@@ -5,17 +5,15 @@ import { OutboundLink } from "gatsby-plugin-google-analytics";
 const ProjectLink = ({ project }) => {
   return (
     <article className="p-3 rounded-md shadow-md bg-white transition duration-500 transform hover:scale-105 hover:shadow-xl">
-      <h3 className="text-2xl font-semibold">
-        <OutboundLink
-          href={project.url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {project.title}
-        </OutboundLink>
-      </h3>
+      <OutboundLink
+        href={project.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h3 className="text-2xl font-semibold">{project.title}</h3>
 
-      <p className="text-base">{project.description}</p>
+        <p className="text-base">{project.description}</p>
+      </OutboundLink>
     </article>
   );
 };
