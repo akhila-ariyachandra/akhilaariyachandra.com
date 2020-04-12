86259b315b0becaaa754a4a4684cd39418a36f39
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-12 16:53:08 +0530

    Add canonical url link

--- gatsby-config.js
+++ gatsby-config.js
@@ -330,6 +330,13 @@ module.exports = {
     `gatsby-plugin-preact`,
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-zeit-now`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://akhilaariyachandra.com`,
+        stripQueryString: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
