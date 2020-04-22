ff22e51df0bcdb156ff86bd14e79e4f276d21473
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-22 15:01:05 +0530

    Fixed grid for technologies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -208,7 +208,7 @@ const Index: React.FunctionComponent = () => {
         </LeftSection>
 
         <RightSection>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
             {technologies.map((technology) => (
               <TechnologyBlock technology={technology} key={technology.name} />
             ))}
