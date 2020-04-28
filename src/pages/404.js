421e795d8beb78defafd4fc4c6b5f129ee36655b
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-28 13:22:37 +0530

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,20 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const NotFoundPage = ({ data, location }) => {
+const NotFoundPage = ({ location }) => {
   return (
     <Layout location={location}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+      <div className="my-48 sm:my-56">
+        <h1 className="text-2xl sm:text-4xl font-semibold">Not Found</h1>
+        <p className="text-base sm:text-xl font-base">
+          You just hit a route that doesn&#39;t exist... the sadness.{" "}
+          <span role="img" aria-label="Sad Emoji">
+            😢
+          </span>
+        </p>
+      </div>
     </Layout>
   );
 };
