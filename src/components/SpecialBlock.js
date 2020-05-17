054911ac7d1974c4b95210de3cb12448d897baeb
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-05-17 22:31:57 +0530

    Changed styling of special block

--- src/components/SpecialBlock.js
+++ src/components/SpecialBlock.js
@@ -4,23 +4,23 @@ import { FaExclamationCircle, FaLightbulb, FaStickyNote } from "react-icons/fa";
 
 const SpecialBlock = ({ children, type }) => {
   let wrapperClass =
-    "text-base sm:text-lg my-3 p-4 rounded-md border-l-4 flex items-start ";
+    "text-base sm:text-lg my-3 p-4 border-l-4 flex items-start ";
   let iconClass = "text-2xl sm:text-3xl ";
   let Icon = null;
 
   switch (type) {
     case "warn":
-      wrapperClass = wrapperClass + "bg-red-200 border-red-600";
+      wrapperClass = wrapperClass + "border-red-600";
       iconClass = iconClass + "text-red-600";
       Icon = FaExclamationCircle;
       break;
     case "info":
-      wrapperClass = wrapperClass + "bg-yellow-200 border-yellow-600";
+      wrapperClass = wrapperClass + "border-yellow-600";
       iconClass = iconClass + "text-yellow-600";
       Icon = FaLightbulb;
       break;
     default:
-      wrapperClass = wrapperClass + "bg-green-200 border-green-600";
+      wrapperClass = wrapperClass + "border-green-600";
       iconClass = iconClass + "text-green-600";
       Icon = FaStickyNote;
   }
@@ -29,9 +29,7 @@ const SpecialBlock = ({ children, type }) => {
     <div className={wrapperClass}>
       <Icon className={iconClass} />
 
-      <div className="text-black text-base sm:text-lg flex-1 ml-2 sm:ml-4">
-        {children}
-      </div>
+      <div className="text-base sm:text-lg flex-1 ml-2 sm:ml-4">{children}</div>
     </div>
   );
 };
