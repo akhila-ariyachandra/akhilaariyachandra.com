d9320356824272862c1b54238dad33b5a6950e8e
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-09-20 21:59:36 +0530

    Aded URL Shortener

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,11 +23,11 @@ module.exports = {
         url: `https://sri-lanka-covid-19.now.sh/`,
         description: `COVID-19 Tracker for Sri Lanka`,
       },
-      /* {
-        title: `Blogger`,
-        url: `https://github.com/akhila-ariyachandra/blogger`,
-        description: `A simple blogging site`,
-      }, */
+      {
+        title: `URL Shortener`,
+        url: `https://github.com/akhila-ariyachandra/URL-Shortener`,
+        description: `A simple URL shortener application`,
+      },
     ],
   },
   plugins: [
