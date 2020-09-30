65befd5bedd6dffd89ec3fd04b33f112469a5864
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-09-30 21:18:52 +0530

    Improve hit counter logic

--- src/components/HitCounter.js
+++ src/components/HitCounter.js
@@ -5,13 +5,7 @@ const HitCounter = ({ slug }) => {
   const [hits, setHits] = React.useState(undefined);
 
   React.useEffect(() => {
-    // Don't count hits on localhost
-    if (process.env.NODE_ENV !== "production") {
-      return;
-    }
-
     // Invoke the function by making a request.
-    // Update the URL to match the format of your platform.
     fetch(`/api/register-hit?slug=${slug}`)
       .then((res) => res.json())
       .then((json) => {
