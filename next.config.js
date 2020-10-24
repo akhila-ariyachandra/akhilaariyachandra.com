cc42f6c74ecac783b105a5c59235f4f69abd3977
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-10-24 17:32:32 +0530

    Added bundle size analyzer

--- next.config.js
+++ next.config.js
@@ -1,41 +1,46 @@
 const withOffline = require("next-offline");
 const withOptimizedImages = require("next-optimized-images");
 const path = require("path");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = withOffline(
-  withOptimizedImages({
-    webpack: (config) => {
-      config.resolve.alias.images = path.join(
-        __dirname,
-        "src",
-        "content",
-        "images"
-      );
+module.exports = withBundleAnalyzer(
+  withOffline(
+    withOptimizedImages({
+      webpack: (config) => {
+        config.resolve.alias.images = path.join(
+          __dirname,
+          "src",
+          "content",
+          "images"
+        );
 
-      return config;
-    },
-    workboxOpts: {
-      swDest: "static/service-worker.js",
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: "NetworkFirst",
-          options: {
-            cacheName: "offlineCache",
-            expiration: {
-              maxEntries: 200,
+        return config;
+      },
+      workboxOpts: {
+        swDest: "static/service-worker.js",
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "offlineCache",
+              expiration: {
+                maxEntries: 200,
+              },
             },
           },
-        },
-      ],
-    },
-    async rewrites() {
-      return [
-        {
-          source: "/service-worker.js",
-          destination: "/_next/static/service-worker.js",
-        },
-      ];
-    },
-  })
+        ],
+      },
+      async rewrites() {
+        return [
+          {
+            source: "/service-worker.js",
+            destination: "/_next/static/service-worker.js",
+          },
+        ];
+      },
+    })
+  )
 );
