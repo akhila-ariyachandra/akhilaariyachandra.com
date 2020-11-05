acdaba920debc9be05b60efe82b6d8feab6dea26
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-11-05 17:46:35 +0530

    Use SWR to get page hits

--- src/components/post/HitCounter.tsx
+++ src/components/post/HitCounter.tsx
@@ -1,27 +1,21 @@
 import RetroHitCounter from "react-retro-hit-counter";
-import { FunctionComponent, useState, useEffect } from "react";
+import useSWR from "swr";
+import fetch from "unfetch";
+import { FunctionComponent } from "react";
 import { useRouter } from "next/router";
 
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
 const HitCounter: FunctionComponent = () => {
-  const [hits, setHits] = useState<number>(undefined);
   const router = useRouter();
+  const { data } = useSWR(`/api/register-hit?slug=${router.asPath}`, fetcher, {
+    initialData: { hits: 0 },
+    revalidateOnMount: true,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 
-  useEffect(() => {
-    // Invoke the function by making a request.
-    fetch(`/api/register-hit?slug=${router.asPath}`)
-      .then((res) => res.json())
-      .then((json) => {
-        if (typeof json.hits === "number") {
-          setHits(json.hits);
-        }
-      });
-  }, [router.asPath]);
-
-  if (typeof hits === "undefined") {
-    return null;
-  }
-
-  return <RetroHitCounter hits={hits} />;
+  return <RetroHitCounter hits={data.hits} />;
 };
 
 export default HitCounter;
