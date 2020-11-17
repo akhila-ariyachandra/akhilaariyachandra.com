5d7af3f56bc3b9e26eb20a2e667272ca6ada66c5
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-11-17 17:30:46 +0530

    Fix options for next-pwa

--- next.config.js
+++ next.config.js
@@ -11,9 +11,11 @@ module.exports = withPlugins(
     [
       withPWA,
       {
-        dest: "public",
-        runtimeCaching,
-        disable: process.env.NODE_ENV === "development",
+        pwa: {
+          dest: "public",
+          runtimeCaching,
+          disable: process.env.NODE_ENV === "development",
+        },
       },
     ],
   ],
