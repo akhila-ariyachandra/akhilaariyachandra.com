c68b4c2e36cb9f2ef22893df49714e8d30c5c68e
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-12-01 17:39:34 +0530

    Remove react-lazyload

--- src/components/code/Code.js
+++ src/components/code/Code.js
@@ -1,7 +1,4 @@
-import useMedia from "use-media";
 import Title from "src/components/code/Title";
-import LazyLoad from "react-lazyload";
-import Loading from "src/components/code/Loading";
 import SyntaxHighlight from "src/components/code/SyntaxHighlight";
 import styles from "src/components/code/Code.module.scss";
 
@@ -17,29 +14,16 @@ const getParams = (className = ``) => {
 };
 
 const Code = ({ children }) => {
-  const isWide = useMedia({ minWidth: "640px" });
-
   const className = children.props.className || "";
   const [language, { title = `` }] = getParams(className);
-  const noOfLines = children.props.children.trim().split(/\r\n|\r|\n/).length;
-  const height = `calc(${noOfLines * (isWide ? 1.8 : 1.75)} + ${
-    2 * (isWide ? 1.111 : 0.857)
-  }em)`;
 
   return (
     <div className={styles.codeBlock}>
       <Title text={title}>{language}</Title>
 
-      <LazyLoad
-        style={{ height }}
-        placeholder={<Loading noOfLines={noOfLines} height={height} />}
-        once
-        offset={100}
-      >
-        <SyntaxHighlight title={title} language={language}>
-          {children}
-        </SyntaxHighlight>
-      </LazyLoad>
+      <SyntaxHighlight title={title} language={language}>
+        {children}
+      </SyntaxHighlight>
     </div>
   );
 };
