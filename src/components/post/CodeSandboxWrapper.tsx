0e8270db14f8982d42bcea0dc792d46cf2d49ed0
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2020-12-30 23:12:01 +0530

    Don't use dynamic import for CodeSandbox

--- src/components/post/CodeSandboxWrapper.tsx
+++ src/components/post/CodeSandboxWrapper.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import dynamic from "next/dynamic";
-const CodeSandbox = dynamic(() =>
-  import("mdx-embed").then((mod) => mod.CodeSandbox)
-);
+import { CodeSandbox } from "mdx-embed";
 
 type Props = {
   codeSandboxId: string;
