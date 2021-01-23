583ebdfbe689e7f4b5deefdfa1b19badb152b2d6
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-01-23 23:24:10 +0530

    Switch to Preact

--- next.config.js
+++ next.config.js
@@ -68,5 +68,17 @@ module.exports = withPlugins(
 
       return redirects;
     },
+    webpack: (config, { dev, isServer }) => {
+      // Replace React with Preact only in client production build
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: "preact/compat",
+          "react-dom/test-utils": "preact/test-utils",
+          "react-dom": "preact/compat",
+        });
+      }
+
+      return config;
+    },
   }
 );
