c77b5e68d3b0b19d43b546db75512927aae20740
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-01-24 09:00:36 +0530

    Removed Preact

--- next.config.js
+++ next.config.js
@@ -68,17 +68,5 @@ module.exports = withPlugins(
 
       return redirects;
     },
-    webpack: (config, { dev, isServer }) => {
-      // Replace React with Preact only in client production build
-      if (!dev && !isServer) {
-        Object.assign(config.resolve.alias, {
-          react: "preact/compat",
-          "react-dom/test-utils": "preact/test-utils",
-          "react-dom": "preact/compat",
-        });
-      }
-
-      return config;
-    },
   }
 );
