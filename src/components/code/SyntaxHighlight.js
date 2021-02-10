b9c2c9f8c5db5f08c5fbc3901587f215e0943c82
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-02-10 19:19:03 +0530

    Try to lazy load code block

--- src/components/code/SyntaxHighlight.js
+++ src/components/code/SyntaxHighlight.js
@@ -1,8 +1,12 @@
 import theme from "prism-react-renderer/themes/nightOwl";
 import rangeParser from "parse-numeric-range";
+import LazyLoad from "react-lazyload";
 import Highlight, { defaultProps } from "prism-react-renderer";
+import { FaCog } from "react-icons/fa";
 
 import styles from "src/components/code/SyntaxHighlight.module.scss";
+
+const LINE_HEIGHT = 1.7142857;
 
 const calculateLinesToHighlight = (meta) => {
   const RE = /{([\d,-]+)}/;
@@ -19,6 +23,7 @@ const SyntaxHighlight = ({ children, title, language }) => {
   const ifTitle = (title || language) && { marginTop: `0px` };
   const metastring = children.props.metastring || "";
   const shouldHighlightLine = calculateLinesToHighlight(metastring);
+  const noOfLines = children.props.children.trim().split(/\r\n|\r|\n/).length;
 
   return (
     <Highlight
@@ -29,24 +34,39 @@ const SyntaxHighlight = ({ children, title, language }) => {
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <div className={`${styles.nextHighlight} horizontalScroll`}>
-          <pre
-            className={`${className} ${styles.prismCode}`}
-            style={{ ...style, ...ifTitle }}
+          <LazyLoad
+            height={`${noOfLines * LINE_HEIGHT}em`}
+            once
+            placeholder={
+              <div
+                style={{
+                  height: `${noOfLines * LINE_HEIGHT}em`,
+                }}
+                className="grid place-items-center"
+              >
+                <FaCog className="text-gray-100 text-lg animate-spin" />
+              </div>
+            }
           >
-            {tokens.map((line, i) => {
-              const lineProps = getLineProps({ line, key: i });
-              if (shouldHighlightLine(i)) {
-                lineProps.className = `${lineProps.className} ${styles.highlightLine}`;
-              }
-              return (
-                <div key={i} {...lineProps}>
-                  {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              );
-            })}
-          </pre>
+            <pre
+              className={`${className} ${styles.prismCode}`}
+              style={{ ...style, ...ifTitle }}
+            >
+              {tokens.map((line, i) => {
+                const lineProps = getLineProps({ line, key: i });
+                if (shouldHighlightLine(i)) {
+                  lineProps.className = `${lineProps.className} ${styles.highlightLine}`;
+                }
+                return (
+                  <div key={i} {...lineProps}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                );
+              })}
+            </pre>
+          </LazyLoad>
         </div>
       )}
     </Highlight>
