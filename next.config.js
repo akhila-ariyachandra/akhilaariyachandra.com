de12134523989c506583ea74fe8dab66efee2e68
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-04-18 15:14:01 +0530

    Try adding security headers

--- next.config.js
+++ next.config.js
@@ -7,6 +7,51 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const fs = require("fs");
 const path = require("path");
 
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' utteranc.es;
+  iframe-src *.now.sh *.vercel.app;
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
 module.exports = withPlugins(
   [
     [withBundleAnalyzer],
@@ -24,6 +69,18 @@ module.exports = withPlugins(
   {
     images: {
       domains: ["i.scdn.co"],
+    },
+    headers: async () => {
+      return [
+        {
+          source: "/",
+          headers: securityHeaders,
+        },
+        {
+          source: "/:path*",
+          headers: securityHeaders,
+        },
+      ];
     },
     rewrites: async () => {
       return [
