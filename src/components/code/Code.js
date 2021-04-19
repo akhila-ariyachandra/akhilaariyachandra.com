39c5f239e785b80d6eafa6c33132bd385b3f294a
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-04-19 09:39:16 +0530

    Revert a059ded2a5624ad624288111aa63c768cd22fd78

--- src/components/code/Code.js
+++ src/components/code/Code.js
@@ -1,5 +1,16 @@
+import dynamic from "next/dynamic";
 import Title from "@/components/code/Title";
-import SyntaxHighlight from "@/components/code/SyntaxHighlight";
+const SyntaxHighlight = dynamic(
+  () => import("@/components/code/SyntaxHighlight"),
+  {
+    loading: () => (
+      <div className="grid place-items-center h-12">
+        <FaCog className="text-gray-100 text-lg animate-spin" />
+      </div>
+    ),
+  }
+);
+import { FaCog } from "react-icons/fa";
 
 import styles from "@/components/code/Code.module.scss";
 
--- src/lib/mdx.tsx
+++ src/lib/mdx.tsx
@@ -1,11 +1,8 @@
-import dynamic from "next/dynamic";
-const SpecialBlock = dynamic(() => import("@/components/post/SpecialBlock"));
-const Code = dynamic(() => import("@/components/code/Code"));
-const PostImage = dynamic(() => import("@/components/post/PostImage"));
-const CodeSandboxWrapper = dynamic(
-  () => import("@/components/post/CodeSandboxWrapper")
-);
-const Iframe = dynamic(() => import("@/components/post/Iframe"));
+import SpecialBlock from "@/components/post/SpecialBlock";
+import Code from "@/components/code/Code";
+import PostImage from "@/components/post/PostImage";
+import CodeSandboxWrapper from "@/components/post/CodeSandboxWrapper";
+import Iframe from "@/components/post/Iframe";
 
 export const mdxComponents = {
   SpecialBlock,
