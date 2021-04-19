22567a8355863ac7455508c58c35f40c22052e0a
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-04-19 22:14:50 +0530

    Added CodeSandbox to security headers

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const path = require("path");
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' utteranc.es;
-  frame-src *.now.sh *.vercel.app utteranc.es;
+  frame-src *.now.sh *.vercel.app utteranc.es codesandbox.io;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
