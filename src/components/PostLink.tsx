0a734db25fe1eaeaaae7c78b5c09a37f7b23b6ce
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-04-19 19:33:28 +0530

    Added views to blog list

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -1,12 +1,19 @@
+import useSWR from "swr";
 import Link from "next/link";
 import type { FunctionComponent } from "react";
 import type { Post } from "@/lib/types";
+import { fetcher, formatDate } from "@/lib/helpers";
 
 type Props = {
   post: Post;
 };
 
 const PostLink: FunctionComponent<Props> = ({ post }) => {
+  const { data } = useSWR(`/api/hit/${post.id}`, fetcher, {
+    initialData: 0,
+    revalidateOnMount: true,
+  });
+
   return (
     <article className="space-y-2">
       <Link href={`/blog/${post.id}`}>
@@ -15,9 +22,13 @@ const PostLink: FunctionComponent<Props> = ({ post }) => {
         </a>
       </Link>
 
-      <p className="dark:text-gray-200 text-gray-800 text-lg font-medium">
-        {post.formattedDate}
-      </p>
+      <div className="flex flex-col dark:text-gray-200 text-gray-800 text-lg font-medium sm:flex-row">
+        <p>{formatDate(post.date)}</p>
+
+        <span className="hidden mx-2 sm:block">-</span>
+
+        <p>{`${data} views`}</p>
+      </div>
     </article>
   );
 };
