ea94df58f7ec014d5f038af8dd98d4f6160c0356
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-04-25 19:59:36 +0530

    Don't hide comments

--- src/components/post/Comments.js
+++ src/components/post/Comments.js
@@ -2,33 +2,26 @@ import React from "react";
 import Head from "next/head";
 
 const Comments = () => {
-  const [visible, setVisible] = React.useState(false);
   const commentBox = React.createRef();
 
-  const showComments = () => {
-    setVisible(true);
-  };
-
   React.useEffect(() => {
-    if (visible) {
-      const scriptEl = document.createElement("script");
+    const scriptEl = document.createElement("script");
 
-      scriptEl.async = true;
-      scriptEl.src = "https://utteranc.es/client.js";
-      scriptEl.setAttribute("repo", process.env.NEXT_PUBLIC_UTTERANCES_REPO);
-      scriptEl.setAttribute("issue-term", "og:title");
-      scriptEl.setAttribute("label", "Comment");
-      scriptEl.setAttribute("theme", "github-light");
-      scriptEl.setAttribute("crossorigin", "anonymous");
-      scriptEl.setAttribute("id", "utterances");
+    scriptEl.async = true;
+    scriptEl.src = "https://utteranc.es/client.js";
+    scriptEl.setAttribute("repo", process.env.NEXT_PUBLIC_UTTERANCES_REPO);
+    scriptEl.setAttribute("issue-term", "og:title");
+    scriptEl.setAttribute("label", "Comment");
+    scriptEl.setAttribute("theme", "github-light");
+    scriptEl.setAttribute("crossorigin", "anonymous");
+    scriptEl.setAttribute("id", "utterances");
 
-      if (commentBox && commentBox.current) {
-        commentBox.current.appendChild(scriptEl);
-      } else {
-        console.log(`Error adding utterances comments on: ${commentBox}`);
-      }
+    if (commentBox && commentBox.current) {
+      commentBox.current.appendChild(scriptEl);
+    } else {
+      console.log(`Error adding utterances comments on: ${commentBox}`);
     }
-  }, [visible]);
+  }, []);
 
   return (
     <React.Fragment>
@@ -41,19 +34,10 @@ const Comments = () => {
         />
       </Head>
 
-      {visible ? (
-        <div
-          className="full-bleed mx-auto my-6 p-2 max-w-screen-md bg-white md:rounded-lg"
-          ref={commentBox}
-        />
-      ) : (
-        <button
-          onClick={showComments}
-          className="full-bleed mx-auto my-6 px-3 py-2 max-w-screen-md text-gray-50 text-lg font-medium bg-green-700 shadow-lg md:rounded-lg"
-        >
-          Show Comments
-        </button>
-      )}
+      <div
+        className="full-bleed mx-auto my-6 p-2 max-w-screen-md bg-white md:rounded-lg"
+        ref={commentBox}
+      />
     </React.Fragment>
   );
 };
