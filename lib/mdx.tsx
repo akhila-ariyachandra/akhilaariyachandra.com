b185e55dbc078ac63d4b6385861ff3a37320eee7
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2021-12-26 14:34:57 +0530

    Remove dynamic import for CodeSandboxWrapper

--- lib/mdx.tsx
+++ lib/mdx.tsx
@@ -1,10 +1,7 @@
-import dynamic from "next/dynamic";
 import SpecialBlock from "@/components/mdx/SpecialBlock";
 import Code from "@/components/mdx/Code";
 import PostImage from "@/components/mdx/PostImage";
-const CodeSandboxWrapper = dynamic(
-  () => import("@/components/mdx/CodeSandboxWrapper")
-);
+import CodeSandboxWrapper from "@/components/mdx/CodeSandboxWrapper";
 import Iframe from "@/components/mdx/Iframe";
 import Video from "@/components/mdx/Video";
 import Reactions from "@/components/post/Reactions";
