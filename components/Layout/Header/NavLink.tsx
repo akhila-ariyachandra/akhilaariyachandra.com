952de5cb289594c093f1fe3f6c9654236a0d1d8d
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2022-01-23 00:10:40 +0530

    Change background of tooltip in dark mode

--- components/Layout/Header/NavLink.tsx
+++ components/Layout/Header/NavLink.tsx
@@ -20,7 +20,7 @@ const NavLink: FC<{
             enabled
               ? "bg-emerald-200 dark:bg-gray-800"
               : "text-gray-800 dark:text-gray-200"
-          } hover:bg-yellow-200 dark:hover:bg-yellow-800`}
+          } hover:bg-yellow-200 dark:hover:bg-emerald-800`}
           onClick={(e) => {
             e.preventDefault();
             router.push(href);
@@ -31,7 +31,7 @@ const NavLink: FC<{
       </Tooltip.Trigger>
 
       <Tooltip.Content sideOffset={10}>
-        <span className="px-2 py-1 bg-yellow-200 dark:bg-yellow-800 leading-none rounded">
+        <span className="px-2 py-1 bg-yellow-200 dark:bg-emerald-800 leading-none rounded">
           {label}
         </span>
       </Tooltip.Content>
