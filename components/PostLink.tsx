e76d0d3020dac1da37a1a1a0f3ce21c92df91317
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2022-08-04 11:07:18 +0530

    Added page views

--- components/PostLink.tsx
+++ components/PostLink.tsx
@@ -1,3 +1,4 @@
+import useViews from "@/hooks/useViews.hook";
 import Link from "next/link";
 import type { FC } from "react";
 import { formatDate } from "@/lib/helpers";
@@ -9,6 +10,8 @@ type PostLinkProps = {
 };
 
 const PostLink: FC<PostLinkProps> = ({ slug, title, date }) => {
+  const { count } = useViews(slug);
+
   return (
     <article className="space-y-2">
       <Link href={`/blog/${slug}`}>
@@ -19,6 +22,10 @@ const PostLink: FC<PostLinkProps> = ({ slug, title, date }) => {
 
       <div className="font-roboto-slab text-lg font-medium text-zinc-800 dark:text-zinc-200">
         {formatDate(date)}
+
+        <span className="mx-2">&bull;</span>
+
+        {`${count} views`}
       </div>
     </article>
   );
