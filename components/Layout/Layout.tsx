6334cfb89400d07dc71274b54c9b1d4146165bc3
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2022-08-11 16:55:20 +0530

    Added border to top of page

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import Footer from "./Footer";
 import Header from "./Header";
 import type { FC, ReactNode } from "react";
@@ -8,7 +9,12 @@ interface Props {
 
 const Layout: FC<Props> = ({ children }) => {
   return (
-    <div className="mx-auto flex min-h-full flex-col justify-between">
+    <div
+      className={classNames(
+        "mx-auto flex min-h-full flex-col justify-between",
+        "border-t-8 border-emerald-600 dark:border-emerald-400"
+      )}
+    >
       <Header />
 
       <main className="container w-full max-w-3xl p-4">{children}</main>
