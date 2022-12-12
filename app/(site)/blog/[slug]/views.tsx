da00bc1e8f9dbff1bec39e32260a179f7bd3d999
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2022-12-12 14:42:08 +0530

    Revert "Added functionality to stop hits increment"
    This reverts commit a1a03ca75de2461ec062bd86b5319244cb1747db.

--- app/(site)/blog/[slug]/views.tsx
+++ app/(site)/blog/[slug]/views.tsx
@@ -2,36 +2,18 @@
 
 import useViews from "@/hooks/useViews.hook";
 import { type FC, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-
-const NO_INCREMENT_KEY = "no-increment-key";
 
 interface BlogPostViewsProps {
   slug: string;
 }
 
 const BlogPostViews: FC<BlogPostViewsProps> = ({ slug }) => {
-  const searchParams = useSearchParams();
   const { count, increment } = useViews(slug);
 
   useEffect(() => {
-    if (typeof document === "undefined") {
-      return;
-    }
-
-    if (searchParams.get("no-increment")) {
-      try {
-        localStorage.setItem(NO_INCREMENT_KEY, "1");
-      } catch {}
-    }
-
-    try {
-      if (localStorage.getItem(NO_INCREMENT_KEY) !== "1") {
-        // Will run twice in development mode due to React Strict mode
-        increment();
-      }
-    } catch {}
-  }, [increment, searchParams]);
+    // Will run twice in development mode due to React Strict mode
+    increment();
+  }, [increment]);
 
   return <p>{`${count} views`}</p>;
 };
