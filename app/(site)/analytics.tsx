b6454e826726d4494eef2d4c939f80248fd80174
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2022-12-12 13:42:51 +0530

    Added DNT for analytics

--- app/(site)/analytics.tsx
+++ app/(site)/analytics.tsx
@@ -1,10 +1,47 @@
 "use client";
 
-import type { FC } from "react";
+import { FC, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import { Analytics } from "@vercel/analytics/react";
 
+const DNT_KEY = "vercel-analytics-dnt";
+
+export const setStorageSafe = (key: string, value: string) => {
+  try {
+    localStorage.setItem(key, value);
+  } catch {}
+};
+
+export const getStorageSafe = (key: string) => {
+  try {
+    return localStorage.getItem(key);
+  } catch {}
+};
+
 const AnalyticsWrapper: FC = () => {
-  return <Analytics />;
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof document === "undefined") {
+      return;
+    }
+
+    if (searchParams.get("dnt")) {
+      setStorageSafe(DNT_KEY, "1");
+    }
+  }, [searchParams]);
+
+  return (
+    <Analytics
+      beforeSend={(e) => {
+        if (getStorageSafe(DNT_KEY) === "1") {
+          return null;
+        }
+
+        return e;
+      }}
+    />
+  );
 };
 
 export default AnalyticsWrapper;
