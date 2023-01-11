e586ba89b37fd5e1e10f7f0d8592af32f7e2ce99
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-01-11 08:09:55 +0530

    Upgraded dependencies

--- app/(site)/providers.tsx
+++ app/(site)/providers.tsx
@@ -4,6 +4,7 @@ import { type ReactNode, type FC, useState } from "react";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider as BalancerProvider } from "react-wrap-balancer";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -20,7 +21,7 @@ const Providers: FC<ProvidersProps> = ({ children }) => {
         attribute="class"
         enableSystem={false}
       >
-        {children}
+        <BalancerProvider>{children}</BalancerProvider>
       </ThemeProvider>
 
       <ReactQueryDevtools />
