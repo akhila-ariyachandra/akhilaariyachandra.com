c75ebfb43e4e8fc2827a16e393a78ddcca5f7c53
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-03-06 16:55:14 +0530

    Revert "Added rate limiter"
    This reverts commit da37ff1fbf78b63e2fc012c63586ba9079d24a05.

--- app/views/[slug]/route.ts
+++ app/views/[slug]/route.ts
@@ -1,15 +1,7 @@
 import prisma from "@/prisma";
-import redis from "@/lib/redis";
 import { NextResponse } from "next/server";
-import { Ratelimit } from "@upstash/ratelimit";
 
 export const dynamic = "force-dynamic";
-
-const ratelimit = new Ratelimit({
-  redis: redis,
-  limiter: Ratelimit.slidingWindow(1, "60 s"), // 1 request per 60 seconds
-  analytics: true,
-});
 
 interface Options {
   params: {
@@ -43,22 +35,6 @@ export const GET = async (request: Request, { params }: Options) => {
 
 export const POST = async (request: Request, { params }: Options) => {
   const slug = params.slug;
-
-  const ip =
-    request.headers["x-real-ip"] ??
-    request.headers["x-forwarded-for"] ??
-    "localhost:3000";
-
-  const { success } = await ratelimit.limit(ip);
-
-  if (!success) {
-    return NextResponse.json(
-      {
-        error: "Too many requests",
-      },
-      { status: 429 }
-    );
-  }
 
   const views = await prisma.views.upsert({
     create: {
