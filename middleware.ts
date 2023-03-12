375998df13b8cdc5afb96c86775a2fc3a91fe696
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-03-12 18:26:53 +0530

    Re-enable rate limiter

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,52 @@
-import redis from "@/lib/redis";
-import { type NextRequest, NextResponse } from "next/server";
+import {
+  type NextRequest,
+  NextResponse,
+  type NextFetchEvent,
+} from "next/server";
+import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
 
 const ratelimit = new Ratelimit({
-  redis: redis,
-  limiter: Ratelimit.slidingWindow(1, "60 s"), // 1 request per 60 seconds
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.cachedFixedWindow(1, "1 m"),
   analytics: true,
+  ephemeralCache: new Map(),
 });
 
 export const config = {
   matcher: "/views/:slug*",
 };
 
-export const middleware = async (request: NextRequest) => {
+export const middleware = async (
+  request: NextRequest,
+  event: NextFetchEvent
+) => {
   // Limit blog post views increment
-  /* if (request.method === "POST") {
-    const ip = request.ip ?? "localhost:3000";
+  if (request.method === "POST") {
+    const ip = request.ip ?? "127.0.0.1";
     const slug = request.nextUrl.pathname.replace("/views/", "");
 
-    const { success } = await ratelimit.limit(`${ip}_${slug}`);
+    const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+      `${ip}_${slug}`,
+      request
+    );
+    event.waitUntil(pending);
 
-    if (!success) {
-      return NextResponse.json(
-        {
-          error: "Too many requests",
-        },
-        { status: 429 }
-      );
-    }
-  } */
+    const res = success
+      ? NextResponse.next()
+      : NextResponse.json(
+          {
+            error: "Too many requests",
+          },
+          { status: 429 }
+        );
+
+    res.headers.set("X-RateLimit-Limit", limit.toString());
+    res.headers.set("X-RateLimit-Remaining", remaining.toString());
+    res.headers.set("X-RateLimit-Reset", reset.toString());
+
+    return res;
+  }
 
   return NextResponse.next();
 };
