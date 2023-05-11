6a0083ab4a016bb6b32bdea0b924569b2a39bb80
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-05-11 10:44:07 +0530

    Revert "Try adding `revalidate`"
    This reverts commit 1724c5801cf43f4812bf4c84bd40dea4a7579204.

--- app/spotify/now-playing/route.ts
+++ app/spotify/now-playing/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import { getNowPlaying } from "@/lib/spotify";
 
 export const runtime = "edge";
-export const revalidate = 60;
 
 export const GET = async () => {
   const response = await getNowPlaying();
