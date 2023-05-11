1724c5801cf43f4812bf4c84bd40dea4a7579204
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-05-11 10:40:05 +0530

    Try adding `revalidate`

--- app/spotify/now-playing/route.ts
+++ app/spotify/now-playing/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { getNowPlaying } from "@/lib/spotify";
 
 export const runtime = "edge";
+export const revalidate = 60;
 
 export const GET = async () => {
   const response = await getNowPlaying();
