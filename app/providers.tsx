29f7bc710b8cb904b71ad57d6b7962e6fb730834
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-08-23 22:25:33 +0530

    Upgraded dependencies

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,6 @@ import dynamic from "next/dynamic";
 import { type ReactNode, useState } from "react";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider as BalancerProvider } from "react-wrap-balancer";
 import { SessionProvider } from "next-auth/react";
@@ -23,20 +22,18 @@ const Providers = ({ children }: ProvidersProps) => {
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryStreamedHydration>
-          <ThemeProvider
-            defaultTheme="dark"
-            themes={["light", "dark"]}
-            attribute="class"
-            enableSystem={false}
-          >
-            <BalancerProvider>
-              {children}
+        <ThemeProvider
+          defaultTheme="dark"
+          themes={["light", "dark"]}
+          attribute="class"
+          enableSystem={false}
+        >
+          <BalancerProvider>
+            {children}
 
-              <Toaster position="bottom-center" reverseOrder={false} />
-            </BalancerProvider>
-          </ThemeProvider>
-        </ReactQueryStreamedHydration>
+            <Toaster position="bottom-center" reverseOrder={false} />
+          </BalancerProvider>
+        </ThemeProvider>
 
         <ReactQueryDevtools />
       </QueryClientProvider>
