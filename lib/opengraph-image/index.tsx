a60f6c9078959ea943d232bb7eddb65df22f0319
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-09-05 21:18:40 +0530

    Please work

--- lib/opengraph-image/index.tsx
+++ lib/opengraph-image/index.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import "server-only";
-
 import { ImageResponse } from "next/server";
 
 export const getBaseURL = () => {
