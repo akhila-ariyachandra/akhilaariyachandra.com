a439a0ad5221115779c534f287af825866aa9c60
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-09-06 12:51:58 +0530

    Redesign
    * Redesign (#177)
    
    * Start working on redesign
    
    * More changes
    
    * More changes
    
    * Changes
    
    * changes
    
    * More changes
    
    * Disable edge
    
    * Revert "Disable edge"
    
    This reverts commit 6ce362f765f8b74fc0e7cd1f1d6cfe351d9cecbc.
    
    * Remove file
    
    * changes
    
    * Please work
    
    * Revert "Please work"
    
    This reverts commit a60f6c9078959ea943d232bb7eddb65df22f0319.
    
    * Refactor
    
    * Upgraded dependencies
    
    * Remove some plugins
    
    * Revert "Remove some plugins"
    
    This reverts commit 92a228fda567bc705ee655a29d950a6c63b25a77.
    
    * Try removing code hike
    
    * Completely remove @code-hike/mdx
    
    * Revert "Completely remove @code-hike/mdx"
    
    This reverts commit e5be80af1f56f474b85e88b978900cfbf0d512e1.
    
    * Remove font files
    
    * Revert "Try removing code hike"
    
    This reverts commit cbdaf101ee5ee4b3eb01112ba114fbbc976c7a1e.
    
    * Revert "Revert "Try removing code hike""
    
    This reverts commit e310ce242fcbab8e075d0d109a651d0bddeb98db.
    
    * Revert "Remove font files"
    
    This reverts commit 35d502cd8f96e30cad5e853aa73206cd3ee3c30a.
    
    * Revert "Revert "Completely remove @code-hike/mdx""
    
    This reverts commit 81e14e5efe6bf544215f9e4a116b910d008324e1.
    
    * Revert "Completely remove @code-hike/mdx"
    
    This reverts commit e5be80af1f56f474b85e88b978900cfbf0d512e1.
    
    * Revert "Try removing code hike"
    
    This reverts commit cbdaf101ee5ee4b3eb01112ba114fbbc976c7a1e.
    
    * Revert "Revert "Remove some plugins""
    
    This reverts commit 38a1fbb26b9ea52ce05b3dda45fec72dfb270609.
    
    * Revert "Remove some plugins"
    
    This reverts commit 92a228fda567bc705ee655a29d950a6c63b25a77.
    
    * Revert "Upgraded dependencies"
    
    This reverts commit 048f512679b07e48d0af22673202fc1e0e755312.
    
    * Revert "Refactor"
    
    This reverts commit c2800e1068eb8da5f471b37b27e74e3f532eb9de.
    
    * Remove fonts
    
    * Fix font file
    
    * Revert "Fix font file"
    
    This reverts commit bfe4cef3252c9bceb810e4f01857e2d5c4ca9715.
    
    * Revert "Remove fonts"
    
    This reverts commit dcdbaa5433f7d608d87cb468faf4442d1b9cb454.
    
    * Revert "Revert "Refactor""
    
    This reverts commit 1a5ac28bfcaadbc07fd867b18e80ad65e514daba.
    
    * Remove fonts
    
    * Add multiple OG image sizes
    
    * Added ID for caching
    
    * Changed Twitter OG image size
    
    * Fixed route handler
    
    * Set description
    
    * Remove all banner files
    
    * Added dark mode
    
    * Added icons
    
    * Added analytics
    
    * Updated README.md
    
    * Silence Vercel
    
    * Cleanup
    
    * Added back some details
    
    * Complete migration to new table
    
    * Remove redirect from old pages
    
    * Added back missing route

---
+++ app/api/views/[slug]/route.tsx
@@ -0,0 +1,82 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { allPosts } from ".contentlayer/generated";
+import { db } from "@/db/connection";
+import { posts } from "@/db/schema";
+
+export const runtime = "nodejs";
+
+type Options = {
+  params: {
+    slug: string;
+  };
+};
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.cachedFixedWindow(1, "1 m"),
+  analytics: true,
+  ephemeralCache: new Map(),
+});
+
+export const POST = async (request: NextRequest, { params }: Options) => {
+  const ip = request.ip ?? "127.0.0.1";
+  const slug = params.slug;
+
+  if (!allPosts.map((post) => post.slug).includes(slug)) {
+    return NextResponse.json(
+      {
+        error: "Not found",
+      },
+      {
+        status: 404,
+      },
+    );
+  }
+
+  // Check rate limit
+  const { success, limit, reset, remaining } = await ratelimit.limit(
+    `${ip}_${slug}`,
+    request,
+  );
+  if (!success) {
+    return NextResponse.json(
+      {
+        error: "Too many requests",
+      },
+      {
+        status: 429,
+        headers: {
+          "X-RateLimit-Limit": limit.toString(),
+          "X-RateLimit-Remaining": remaining.toString(),
+          "X-RateLimit-Reset": reset.toString(),
+        },
+      },
+    );
+  }
+
+  const result = await db.select().from(posts).where(eq(posts.slug, slug));
+
+  if (result.length === 0) {
+    // Create new record
+    await db.insert(posts).values({
+      slug,
+      views: 1,
+    });
+  } else {
+    // Update record
+    await db
+      .update(posts)
+      .set({ views: result[0].views + 1 })
+      .where(eq(posts.slug, slug));
+  }
+
+  // Revalidate pages
+  revalidatePath("/blog");
+  revalidatePath(`/blog/${slug}`);
+
+  return NextResponse.json({ message: "Incremented" });
+};
