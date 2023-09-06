9bf95a4f991a265dd2b4f29cf69e80744b7ff1e2
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-09-06 13:19:20 +0530

    Disabled `typedRoutes`

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
   images: {
     formats: ["image/avif", "image/webp"],
   },
