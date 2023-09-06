ebcfa793a371b0115381bca7cc0063392d7f9226
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-09-06 23:00:57 +0530

    Use Next.js's own error boundary

---
+++ app/error.tsx
@@ -0,0 +1,29 @@
+"use client"; // Error components must be Client Components
+
+import Title from "@/components/Title";
+import { useEffect } from "react";
+
+const ErrorPage = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <Title>Error</Title>
+
+      <p className="text-sm text-zinc-700 dark:text-zinc-300 sm:text-base">
+        Something went wrong. Maybe try refreshing the page?
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
