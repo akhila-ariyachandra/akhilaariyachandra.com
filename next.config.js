8367bbe4c8d3686a3b570dcff4425c32fcb83886
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-09-08 19:32:16 +0530

    Added back old redirects

--- next.config.js
+++ next.config.js
@@ -9,17 +9,36 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
   },
   redirects: async () => {
-    const directoryPath = path.join(__dirname, "content", "posts");
+    const redirects = [];
 
-    const files = fs.readdirSync(directoryPath);
-    const slugs = files.map((file) => file.replace(".mdx", ""));
+    // Move all blog posts under /blog
+    const postsDirectory = path.join("content", "posts");
+    const fileNames = fs.readdirSync(postsDirectory);
+    const routes = fileNames.map((fileName) => fileName.replace(/\.mdx$/, ""));
 
-    // Redirect old blog post links
-    return slugs.map((slug) => ({
-      source: `/${slug}`,
-      destination: `/blog/${slug}`,
+    for (const route of routes) {
+      redirects.push({
+        source: `/${route}`,
+        destination: `/blog/${route}`,
+        permanent: true,
+      });
+    }
+
+    // Fix redirect for DEV post for https://dev.to/akhilaariyachandra/mimic-react-life-cycle-methods-with-hooks-286a
+    redirects.push({
+      source: `/mimic-react-life-cycles-methods-with-hooks`,
+      destination: `/blog/mimic-react-life-cycle-methods-with-hooks`,
       permanent: true,
-    }));
+    });
+
+    // Rename react-usereducer-with-context to react-usereducer-with-usecontext
+    redirects.push({
+      source: `/blog/react-usereducer-with-context`,
+      destination: `/blog/react-usereducer-with-usecontext`,
+      permanent: true,
+    });
+
+    return redirects;
   },
 };
 
