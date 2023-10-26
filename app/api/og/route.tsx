12349c8e21648605096a22bc52d17e0226a442f6
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-10-26 23:29:20 +0530

    Upgrade to Next.js 14

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse, NextResponse, type NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+import { NextResponse, type NextRequest } from "next/server";
 import { z } from "zod";
 
 import { getBaseURL } from "@/lib/helpers";
