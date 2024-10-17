83146b3e547d0bf760da5e8badac51ecf55873f5
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2024-10-17 11:20:19 +0530

    Try this

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       fullUrl: true,
     },
   },
-  transpilePackages: ["@content-collections/core"],
+  serverExternalPackages: ["@content-collections/core"],
   redirects: async () => {
     // Redirect old snippet pages
     return [
