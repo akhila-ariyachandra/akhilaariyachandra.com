ef38d8d94fbdce52e647e03ddf5c59fced207c86
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2024-12-10 18:33:03 +0530

    Added theme switcher to comments

--- app/blog/[slug]/comments.tsx
+++ app/blog/[slug]/comments.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Giscus from "@giscus/react";
+import { useTheme } from "next-themes";
 
 const Comments = () => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <Giscus
       id="comments"
@@ -15,7 +18,7 @@ const Comments = () => {
       reactionsEnabled="1"
       emitMetadata="0"
       inputPosition="top"
-      theme="light"
+      theme={resolvedTheme === "light" ? "light" : "dark"}
       lang="en"
       loading="lazy"
     />
