119b1658a287c95d7c310ea3583112d73baf7633
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2024-12-11 18:02:29 +0530

    Fixed linting issue

--- app/_db/schema.ts
+++ app/_db/schema.ts
@@ -1,6 +1,7 @@
 import { InferSelectModel } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+// eslint-disable-next-line @typescript-eslint/no-deprecated
 export const post = sqliteTable("post", {
   slug: text("slug").primaryKey(),
   views: integer("views").default(0).notNull(),
