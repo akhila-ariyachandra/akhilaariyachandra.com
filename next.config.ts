cd3873191918fb86f03d00a04fd88df30ee4d130
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2025-07-15 18:26:48 +0530

    Upgrade to Next.jss 15.4 and enable some 16 flags

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
-    dynamicIO: true,
+
+    // Forward browser logs to the terminal for easier debugging
+    browserDebugInfoInTerminal: true,
+
+    // Enable new caching and pre-rendering behavior
+    dynamicIO: true, // will be renamed to cacheComponents in Next.js 16
+
+    // Activate new client-side router improvements
+    clientSegmentCache: true,
+
+    // Explore route composition and segment overrides via DevTools
+    devtoolSegmentExplorer: true,
+
+    // Enable persistent caching for the turbopack dev server and build.
+    turbopackPersistentCaching: true,
   },
   reactStrictMode: true,
   images: {
