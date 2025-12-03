df487dae554ab04b855426d128a3eb26ae7fe463
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2025-12-03 18:13:32 +0530

    Fixed formatting

--- app/_lib/og-image.tsx
+++ app/_lib/og-image.tsx
@@ -16,26 +16,24 @@ export const getOgImage = async ({
   const profilePicSrc = `data:image/png;base64,${profilePicData}`;
 
   return new ImageResponse(
-    (
-      <div tw="flex h-full w-full flex-col justify-between gap-4 bg-zinc-900 p-8">
-        <h1 tw="text-8xl leading-none text-zinc-100">{title}</h1>
+    <div tw="flex h-full w-full flex-col justify-between gap-4 bg-zinc-900 p-8">
+      <h1 tw="text-8xl leading-none text-zinc-100">{title}</h1>
 
-        <div tw="flex shrink-0 flex-row items-end gap-4">
-          <p tw="flex-1 text-3xl text-green-500">
-            akhilaariyachandra.com{pathname}
-          </p>
+      <div tw="flex shrink-0 flex-row items-end gap-4">
+        <p tw="flex-1 text-3xl text-green-500">
+          akhilaariyachandra.com{pathname}
+        </p>
 
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={profilePicSrc}
-            alt="Akhila Ariyachandra"
-            width={240}
-            height={240}
-            tw="shrink-0 rounded-xl"
-          />
-        </div>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={profilePicSrc}
+          alt="Akhila Ariyachandra"
+          width={240}
+          height={240}
+          tw="shrink-0 rounded-xl"
+        />
       </div>
-    ),
+    </div>,
     {
       width: 1200,
       height: 630,
