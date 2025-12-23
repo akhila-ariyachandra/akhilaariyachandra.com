4265e22a7a358a782301365b9a0cc3fb1be8ebaf
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2025-12-23 10:21:36 +0530

    Updated dependencies

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
-    turbopackFileSystemCacheForDev: true,
+    turbopackFileSystemCacheForBuild: true,
     staticGenerationRetryCount: 2,
   },
   reactStrictMode: true,
