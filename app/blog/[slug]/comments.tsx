80e257b74a68d4664f0506368e04977d11d1d7db
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2026-01-11 11:01:32 +0530

    Try adding Bluesky comments

---
+++ app/blog/[slug]/comments.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { BlueskyComments } from "bluesky-comments";
+import "bluesky-comments/bluesky-comments.css";
+
+const Comments = () => {
+  return <BlueskyComments author="akhilaariyachandra.com" />;
+};
+
+export default Comments;
