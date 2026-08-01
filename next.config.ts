67b63c4ccc7d95fa2e9ffd82597e97103357a02b
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2026-08-01 13:24:58 +0530

    Fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    viewTransition: true,
     turbopackFileSystemCacheForBuild: true,
   },
   reactStrictMode: true,
