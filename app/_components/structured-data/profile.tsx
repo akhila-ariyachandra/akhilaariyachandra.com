b62de7836c2b559221e83767bda0760aa6e2e474
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2026-08-13 09:59:33 +0530

    More changes

--- app/_components/structured-data/profile.tsx
+++ app/_components/structured-data/profile.tsx
@@ -1,19 +1,25 @@
-import { PRODUCTION_URL } from "@/_lib/constants";
+import { urlFor } from "@/sanity/lib/image";
+import { sanityFetchStaticParams } from "@/sanity/lib/live";
+import { PERSONAL_INFO_QUERY } from "@/sanity/lib/queries";
 import { type ProfilePage, type WithContext } from "schema-dts";
 
-const jsonLd: WithContext<ProfilePage> = {
-  "@context": "https://schema.org",
-  "@type": "ProfilePage",
-  mainEntity: {
-    "@type": "Person",
-    name: "Akhila Ariyachandra",
-    givenName: "Akhila",
-    familyName: "Ariyachandra",
-    image: `${PRODUCTION_URL}/profile-pic.jpg`,
-  },
-};
+const ProfileStructuredData = async () => {
+  const { data } = await sanityFetchStaticParams({
+    query: PERSONAL_INFO_QUERY,
+  });
 
-const ProfileStructuredData = () => {
+  const jsonLd: WithContext<ProfilePage> = {
+    "@context": "https://schema.org",
+    "@type": "ProfilePage",
+    mainEntity: {
+      "@type": "Person",
+      name: "Akhila Ariyachandra",
+      givenName: "Akhila",
+      familyName: "Ariyachandra",
+      image: data?.picture ? urlFor(data.picture).url() : undefined,
+    },
+  };
+
   return (
     <script
       type="application/ld+json"
