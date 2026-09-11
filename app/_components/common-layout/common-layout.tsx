67ee3c7bbc6f9ad1a053dc6ba79c79efd0e699e2
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2026-09-12 00:57:10 +0530

    Adjusted footer

--- app/_components/common-layout/common-layout.tsx
+++ app/_components/common-layout/common-layout.tsx
@@ -74,10 +74,10 @@ const Footer = async () => {
 
   return (
     <footer className="mx-auto w-full max-w-4xl p-3 sm:p-4">
-      <div className="neobrutalism-container space-y-4 p-3 sm:p-4">
+      <div className="neobrutalism-container space-y-4 py-3 sm:py-4">
         <NowPlaying />
 
-        <div className="flex flex-row items-center justify-between gap-4">
+        <div className="flex flex-row items-center justify-between gap-4 border-t-2 border-t-black px-3 pt-3 sm:px-4 sm:pt-4">
           <p className="text-sm sm:text-base">
             &copy; {year}{" "}
             <Link
