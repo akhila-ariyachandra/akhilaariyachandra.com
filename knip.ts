4f6157af6b6cc7b2113782d03bd6e3e93df9e6e8
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2026-09-19 13:34:15 +0530

    Configure Checkly

--- knip.ts
+++ knip.ts
@@ -2,6 +2,7 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
   project: ["app/**/*.{ts,tsx,css}", "sanity/**/*.{ts,tsx}"],
+  entry: ["checkly.config.ts"],
   ignore: ["sanity/generated/types.ts"],
 };
 
